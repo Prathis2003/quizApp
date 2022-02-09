4d4c24571cd8f3efc55917a5a40f4125ab218fe3
prathis2003 <prathis2003@gmail.com>
2022-02-09 17:07:05 +0530

    question alignment changed

--- lib/questionWidget.dart
+++ lib/questionWidget.dart
@@ -8,15 +8,10 @@ class QuestionCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width/1.5;
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        SizedBox(
-          width: width,
-          child: Text(questionAnswers['questionText'],
-              style: TextStyle(fontSize: 20), textAlign: TextAlign.center),
-        ),
-      ],
+    return Container(
+      width: double.infinity,
+      child: Text(questionAnswers['questionText'],
+          style: TextStyle(fontSize: 20), textAlign: TextAlign.center),
     );
   }
 }
